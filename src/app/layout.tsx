30c9ddd054456c7900fb6e1ed0a736bff856c1ad
farhan <chfarhanilyas550@gmail.com>
2025-02-10 07:05:10 +0500

    Created background and main button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import { GridPattern } from "../components/magicui/gridPattern";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -27,6 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        < GridPattern width={40} height={40} className="-z-10 opacity-85"/>
         {children}
       </body>
     </html>
